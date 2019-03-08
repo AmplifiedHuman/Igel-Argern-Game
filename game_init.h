dd22b6282ca8c54ec7e11417c6e8d7ae5437aff4
AmplifiedHuman@gmail.com <AmplifiedHuman@gmail.com>
2019-03-08 11:52:29 +0000

    Beautified coding style

--- game_init.h
+++ game_init.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   newfile.h
  * Author: lpasqua
  *
@@ -18,28 +18,22 @@
 
 
 //types of squares
-enum stype{
-    NORMAL, OBSTACLE };
-
+enum stype{ NORMAL, OBSTACLE };
 //colors of tokens
-enum color {
-    RED, BLU, GREEN, YELLOW, PINK, ORANGE
-};
-
-//defines a token. 
+enum color { RED, BLUE, GREEN, YELLOW, PINK, ORANGE };
+//defines a token.
 //Note each token can be associated with a color
-typedef struct token{
-   enum color col; 
-    
-}token;
+typedef struct token {
+  enum color col;
+} token;
 
 //Defines a square of the board.
-typedef struct square{
-    //A square can be a NORMAL or an OBSTACLE square
-     enum stype type;
-     //the stack of tokens that can be placed on the board square
-     token * stack;      
-}square;
+typedef struct square {
+  //A square can be a NORMAL or an OBSTACLE square
+  enum stype type;
+  //the stack of tokens that can be placed on the board square
+  token *stack;
+} square;
 
 
 
@@ -48,15 +42,15 @@ typedef struct square{
  * with the information about the player
  * such as a name and a color.
  */
-typedef struct player{
-    
-    
-}player; 
+typedef struct player {
+
+  ;
+} player;
 
 
 /*
  * This function creates the board for the first time
- * 
+ *
  * Input: board - a 6x9 array of squares
  *
  */
@@ -64,12 +58,9 @@ void initialize_board(square board[NUM_ROWS][NUM_COLUMNS]);
 
 /*
  * This function creates players for the first time
- * 
+ *
  * Input: the array of players to be initialized
  * Output: The number of players of the game
  *
  */
 int initialize_players(player players[]);
-
-
-
